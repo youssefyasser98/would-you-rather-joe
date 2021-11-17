03155bfc3c01b7ebfd7aceabb58973224f8a68c1
youssefyasser98 <80383371+youssefyasser98@users.noreply.github.com>
2021-11-17 15:01:12 +0200

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -21,116 +21,24 @@ class Nav extends Component {
     const { authUser, users } = this.props;
 
     return (
-      <Container>
-        <Responsive as={Menu} minWidth={651} pointing secondary>
-          <Menu.Item name="home" as={NavLink} to="/" exact />
-          <Menu.Item name="new poll" as={NavLink} to="/add" />
-          <Menu.Item name="leader board" as={NavLink} to="/leaderboard" />
-          <Menu.Menu position="right">
-            <Menu.Item>
-              <span>
-                <Image
-                  src={users[authUser].avatarURL}
-                  avatar
-                  spaced="right"
-                  verticalAlign="bottom"
-                />
-                {users[authUser].name}
-              </span>
-            </Menu.Item>
-            <Menu.Item>
-              <Button
-                content="Logout"
-                labelPosition="right"
-                basic
-                compact
-                icon="log out"
-                size="mini"
-                onClick={this.handleLogout}
-              />
-            </Menu.Item>
-          </Menu.Menu>
-        </Responsive>
-        <Responsive as={Fragment} minWidth={375} maxWidth={650}>
-          <Grid columns={2} padded="vertically">
-            <Grid.Row>
-              <Grid.Column>
-                <Image
-                  src={users[authUser].avatarURL}
-                  avatar
-                  spaced="right"
-                  verticalAlign="bottom"
-                />
-                {users[authUser].name}
-              </Grid.Column>
-              <Grid.Column verticalAlign="bottom" textAlign="right">
-                <Button
-                  content="Logout"
-                  labelPosition="right"
-                  basic
-                  compact
-                  icon="log out"
-                  size="mini"
-                  onClick={this.handleLogout}
-                />
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-              <Grid.Column width={16}>
-                <Menu pointing secondary widths={3}>
-                  <Menu.Item name="home" as={NavLink} to="/" exact />
-                  <Menu.Item name="new poll" as={NavLink} to="/add" />
-                  <Menu.Item
-                    name="leader board"
-                    as={NavLink}
-                    to="/leaderboard"
-                  />
-                </Menu>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Responsive>
-        <Responsive as={Fragment} maxWidth={374}>
-          <Grid padded="vertically" columns={1}>
-            <Grid.Row>
-              <Grid.Column>
-                <Image
-                  src={users[authUser].avatarURL}
-                  avatar
-                  spaced="right"
-                  verticalAlign="bottom"
-                />
-                {users[authUser].name}
-                <Button
-                  content="Logout"
-                  labelPosition="right"
-                  basic
-                  compact
-                  icon="log out"
-                  size="mini"
-                  floated="right"
-                  onClick={this.handleLogout}
-                />
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-              <Grid.Column>
-                <Menu pointing secondary widths={3}>
-                  <Menu.Item name="home" as={NavLink} to="/" exact />
-                  <Menu.Item name="new poll" as={NavLink} to="/add" />
-                  <Menu.Item
-                    name="leader board"
-                    as={NavLink}
-                    to="/leaderboard"
-                  />
-                </Menu>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Responsive>
-      </Container>
-    );
-  }
+    
+     <nav className='nav'>
+      <ul>
+        <li>
+          <NavLink to='/' exact activeClassName='active'>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/new' activeClassName='active'>
+            New Poll
+          </NavLink>
+        </li>
+      </ul>
+     </nav>
+  )
+}
+    
 }
 
 function mapStateToProps({ users, authUser }) {
